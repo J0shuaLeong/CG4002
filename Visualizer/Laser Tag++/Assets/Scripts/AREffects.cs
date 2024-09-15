c48d7f2110e7b91737b0c98ca7eb28266944b0b0
Feli <feliciabeatrice7@gmail.com>
2024-09-15 21:20:04 +0800

    add throw controller

--- Visualizer/Laser Tag++/Assets/Scripts/AREffects.cs
+++ Visualizer/Laser Tag++/Assets/Scripts/AREffects.cs
@@ -4,4 +4,48 @@ using UnityEngine;
 
 public class AREffects : MonoBehaviour {
     
+    [Header("References")]
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private GameObject objectToThrow;
+
+    [Header("Throwing")]
+    [SerializeField] private KeyCode testThrowKey = KeyCode.F;
+    [SerializeField] private float throwForce;
+    [SerializeField] private float throwUpwardForce;
+
+    bool readyToThrow;
+
+
+    private void Start() {
+        readyToThrow = true;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(testThrowKey) && readyToThrow) {
+            Throw();
+        }
+    }
+
+
+    private void Throw() {
+        readyToThrow = false;
+
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 500f)) {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        readyToThrow = true;
+    }
+
 }
--- Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
+++ Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
@@ -5,20 +5,29 @@ using UnityEngine.XR.ARFoundation;
 
 public class OpponentDetection : MonoBehaviour {
 
-    [SerializeField] ARTrackedImageManager arTrackedImageManager;
+    [SerializeField] private ARTrackedImageManager arTrackedImageManager;
+
+    private bool isVisible;
+
+
+    private void Start() {
+        isVisible = false;
+    }
+
 
     private void OnEnable() => arTrackedImageManager.trackedImagesChanged += OnChanged;
     private void OnDisable() => arTrackedImageManager.trackedImagesChanged -= OnChanged;
 
     private void OnChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        isVisible = true;
+        Debug.Log("isVisible: " + isVisible); // DEBUG
+
         foreach (var newImage in eventArgs.added) {
-            // DEBUG
-            Debug.Log("new image:" + newImage.transform);
+            Debug.Log("new image:" + newImage.transform); // DEBUG
         }
 
         foreach (var updatedImage in eventArgs.updated){
-            // DEBUG
-            Debug.Log("updated image:" + updatedImage.transform);
+            Debug.Log("updated image:" + updatedImage.transform); // DEBUG
         }
     }
 }
