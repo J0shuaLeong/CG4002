5b8d648788b127fa88677b75e8575f7c070d925a
Feli <feliciabeatrice7@gmail.com>
2024-09-15 21:27:07 +0800

    fix opponent detection class

--- Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
+++ Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class OpponentDetection : MonoBehaviour {
 
@@ -23,11 +24,25 @@ public class OpponentDetection : MonoBehaviour {
         Debug.Log("isVisible: " + isVisible); // DEBUG
 
         foreach (var newImage in eventArgs.added) {
-            Debug.Log("new image:" + newImage.transform); // DEBUG
+            if (newImage.trackingState == TrackingState.Tracking) {
+                isVisible = true;
+                Debug.Log("Opponent is visible: " + isVisible + " with transform: " + newImage.transform); // DEBUG
+            }
         }
 
-        foreach (var updatedImage in eventArgs.updated){
-            Debug.Log("updated image:" + updatedImage.transform); // DEBUG
+        foreach (var updatedImage in eventArgs.updated) {
+            if (updatedImage.trackingState == TrackingState.Tracking) {
+                isVisible = true;
+                Debug.Log("Opponent is visible: " + isVisible + " with transform: " + updatedImage.transform); // DEBUG
+            } else {
+                isVisible = false;
+                Debug.Log("Opponent moved out of view: " + isVisible); // DEBUG
+            }
+        }
+
+        foreach (var removedImage in eventArgs.removed) {
+            isVisible = false;
+            Debug.Log("Opponent removed: " + isVisible); // DEBUG
         }
     }
 }
