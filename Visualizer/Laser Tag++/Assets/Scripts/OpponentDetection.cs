f36a11ae2e87309066eb1c54af958f684b334bed
Feli <feliciabeatrice7@gmail.com>
2024-09-15 17:41:19 +0800

    change opponent detection to detect tracked image

--- Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
+++ Visualizer/Laser Tag++/Assets/Scripts/OpponentDetection.cs
@@ -3,62 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-// this might not work, maybe use image tracking instead?
 public class OpponentDetection : MonoBehaviour {
 
-    [SerializeField] private ARHumanBodyManager humanBodyManager;
+    [SerializeField] ARTrackedImageManager arTrackedImageManager;
 
-    private ARHumanBody opponent;
-    public bool isOpponentVisible;
-    // TODO: some variable to determine the coordinates of the opponent
+    private void OnEnable() => arTrackedImageManager.trackedImagesChanged += OnChanged;
+    private void OnDisable() => arTrackedImageManager.trackedImagesChanged -= OnChanged;
 
-
-    private void Start() {
-        opponent = null;
-        isOpponentVisible = false;
-    }
-
-    private void OnEnable() {
-        if (humanBodyManager != null) {
-            humanBodyManager.humanBodiesChanged += OnHumanBodiesChanged;
-        }
-    }
-
-    private void OnDisable() {
-        if (humanBodyManager != null) {
-            humanBodyManager.humanBodiesChanged -= OnHumanBodiesChanged;
-        }
-    }
-
-
-    private void OnHumanBodiesChanged(ARHumanBodiesChangedEventArgs eventArgs) {
-        List<ARHumanBody> humanBodies = eventArgs.added;
-        humanBodies.AddRange(eventArgs.updated);
-
-        opponent = GetClosestHumanBody(humanBodies);
-        
-        if (opponent != null) {
-            isOpponentVisible = true;
-        }
-    }
-
-    private ARHumanBody GetClosestHumanBody(List<ARHumanBody> humanBodies) {
-        if (humanBodies == null || humanBodies.Count == 0) {
-            return null;
+    private void OnChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        foreach (var newImage in eventArgs.added) {
+            // DEBUG
+            Debug.Log("new image:" + newImage.transform);
         }
 
-        ARHumanBody closestHumanBody = null;
-        float closestDistance = float.MaxValue;
-
-        foreach (var humanBody in humanBodies) {
-            float distance = Vector3.Distance(Camera.main.transform.position, humanBody.pose.position);
-            if (distance < closestDistance) {
-                closestDistance = distance;
-                closestHumanBody = humanBody;
-            }
+        foreach (var updatedImage in eventArgs.updated){
+            // DEBUG
+            Debug.Log("updated image:" + updatedImage.transform);
         }
-
-        return closestHumanBody;
     }
-
 }
